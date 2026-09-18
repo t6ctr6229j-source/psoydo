2741301c04636b03c53553ff75f339dd7530b248
t6ctr6229j-source <t6ctr6229j@privaterelay.appleid.com>
2026-09-18 10:30:29 +0200

    Preserve current-page state in navigation

--- app.js
+++ app.js
@@ -35,8 +35,11 @@
     var home=onHome?'':'./';
     var pricingHref=onHome?'#pricing':'preise.html';
     var productHref=home+'#product', usecasesHref=home+'#usecases', securityHref=home+'#security', deploymentHref=home+'#deployment', architectureHref=home+'#architecture', registerHref=home+'#register';
-    if(nav){nav.innerHTML='<a href="'+productHref+'">Produkt</a><a href="technologie.html">Technologie</a><a href="'+usecasesHref+'">Anwendungsfälle</a><a href="'+securityHref+'">Sicherheit</a><a href="'+deploymentHref+'">Betrieb</a><a href="'+pricingHref+'">Preise</a>';}
-    if(mobile){mobile.innerHTML='<a href="'+productHref+'">Produkt</a><a href="technologie.html">Technologie</a><a href="'+usecasesHref+'">Anwendungsfälle</a><a href="'+architectureHref+'">Architektur</a><a href="'+securityHref+'">Sicherheit</a><a href="'+deploymentHref+'">Betrieb</a><a href="'+pricingHref+'">Preise</a><a href="'+registerHref+'">Registrieren</a>';}
+    var path=window.location.pathname;
+    var onTechnology=/technologie\.html$/.test(path), onPricing=/preise\.html$/.test(path);
+    var techCurrent=onTechnology?' aria-current="page"':'', pricingCurrent=onPricing?' aria-current="page"':'';
+    if(nav){nav.innerHTML='<a href="'+productHref+'">Produkt</a><a href="technologie.html"'+techCurrent+'>Technologie</a><a href="'+usecasesHref+'">Anwendungsfälle</a><a href="'+securityHref+'">Sicherheit</a><a href="'+deploymentHref+'">Betrieb</a><a href="'+pricingHref+'"'+pricingCurrent+'>Preise</a>';}
+    if(mobile){mobile.innerHTML='<a href="'+productHref+'">Produkt</a><a href="technologie.html"'+techCurrent+'>Technologie</a><a href="'+usecasesHref+'">Anwendungsfälle</a><a href="'+architectureHref+'">Architektur</a><a href="'+securityHref+'">Sicherheit</a><a href="'+deploymentHref+'">Betrieb</a><a href="'+pricingHref+'"'+pricingCurrent+'>Preise</a><a href="'+registerHref+'">Registrieren</a>';}
   }
 
   function polishCopy(){
